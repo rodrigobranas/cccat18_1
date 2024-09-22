8d519c9167fcae82ce8f883d31ad2f89bb0d9feb
fabiosimplis <fabio.simplis@gmail.com>
2024-09-22 12:41:59 -0300

    Add Model class

--- src/Signup.ts
+++ src/Signup.ts
@@ -1,6 +1,5 @@
-import crypto from "crypto";
-import { validateCpf } from "./validateCpf";
 import AccountDAO from "./AccountDAO";
+import Account from "./Account";
 
 export default class Signup {
 
@@ -9,14 +8,11 @@ export default class Signup {
   }
   
   async execute(input: any){
-    input.accountId = crypto.randomUUID();
+    
+    const account = Account.create(input.name, input.email, input.cpf, input.carPlate, input.password, input.isPassenger, input.isDriver);
     const accountData = await this.accountDAO.getAccountByEmail(input.email)
     if (accountData) throw new Error("Duplicated account");
-    if (!input.name.match(/[a-zA-Z] [a-zA-Z]+/)) throw new Error("Invalid name");
-    if (!input.email.match(/^(.+)@(.+)$/)) throw new Error("Invalid email");
-    if (!validateCpf(input.cpf)) throw new Error("Invalid CPF");
-    if (input.isDriver && !input.carPlate.match(/[A-Z]{3}[0-9]{4}/)) throw new Error("Invalid Car Plate");
-    await this.accountDAO.saveAccount(input);
+    await this.accountDAO.saveAccount(account);
     return {
       accountId: input.accountId
     };
